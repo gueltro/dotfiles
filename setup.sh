084c8fc784b1147e00a648815eafcca90e96207d
Giulio Gueltrini <gg@blob>
2015-07-14 10:00:55 -0700

    more organizing

--- setup.sh
+++ setup.sh
@@ -1,4 +1,7 @@
 # Execute this for setup
+
+#symlink the new files
 ln -s $HOME/dotfiles/confing $HOME/.dotfiles
-ln -s $HOME/emacs $HOME/.emacs
+ln -s $HOME/dotfiles/emacs $HOME/.emacs
+ln -s $HOME/dotfiles/tmux.conf $HOME/.tmux.conf
 git clone https://github.com/Anthony25/gnome-terminal-colors-solarized.git $HOME/solarized
